18c9cf1de5ba44b7155e08d414c2b37aef5daeaa
piyasha <piyasha2016@gmail.com>
2022-08-26 09:59:46 +0530

    before adding custom module

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -794,6 +794,7 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
 # if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
+
 $databases['default']['default'] = array (
   'database' => 'd9',
   'username' => 'root',
